fa2fb0da82e3a1c61ecd17e9c53ac0dee426893f
TheRoyalIrishman <TheRoyalIrishman@users.noreply.github.com>
2024-02-28 13:48:27 -0500

    Progress?

--- IMD_Lab5_PartA_Team11.cpp
+++ IMD_Lab5_PartA_Team11.cpp
@@ -1,13 +1,13 @@
 /*
- * Lab 5
- * IMD_Demo5_H_Bridge.cpp
+ * Lab 4
+ * IMD_Demo4_Sensor_Integration.cpp
  *
- * Created: 2/26/2024 9:37:00 PM
+ * Created: 2/13/2022 2:47:06 PM
  * Author : Cameron Clarke, Logan York
  */ 
 
-#include <avr/interrupt.h>
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 //Define the codes for TWI actions
 #define TWCR_START 0xA4|(1<<TWIE) //send start condition
@@ -28,60 +28,101 @@ void DummyLoop(uint16_t);
 void UpdateTWIDisplayState();
 void UpdateValues();
 
+//Global variables for display strings
+volatile uint8_t FirstLineStr[21] =  "S1:XXXX             ";
+volatile uint8_t SecondLineStr[21] = "M1:XXX D1:X         ";
+volatile uint8_t ThirdLineStr[21] =  "Button: XXX         ";
+volatile uint8_t FourthLineStr[21] = "                    ";
+
 //Global variables for TWI state and data
 uint8_t display_state =0;
 uint8_t TWI_data = 0;
 uint8_t TWI_slave_address = 0;
 uint8_t TWI_char_index = 0;
 
-//Global variables for display strings
-volatile uint8_t FirstLineStr[21] =  "S1: XXXX            ";
-volatile uint8_t SecondLineStr[21] = "M1: XXX D1: X       ";
-volatile uint8_t ThirdLineStr[21] =  "Button: XXX         ";
-volatile uint8_t FourthLineStr[21] = "                    ";
-
 volatile uint16_t time = 0;
 volatile uint8_t isrHalfSecondCount = 0;
 volatile uint8_t ButtonPressed[4] = "OOM"; // Button will later be "ON " or "OFF"
 uint8_t ON[4] = "ON "; // used since I can't change ButtonPressed char by char...
 uint8_t OFF[4] = "OFF";
-uint8_t direction[2] = "A";
 
 volatile uint16_t adcValueOutputZero = 0; // MUX 0
 volatile uint16_t adcValueOutputOne = 0; // MUX 1
 
-volatile uint8_t readADCL = 0; // just so we can read for the ADC conversion
+volatile uint8_t readADCL = 0;
+
+uint8_t direction[2] = "A";
 
 int main(void)
 {
-	sei(); //Enable Interrupts
-	//Configure Pins
-	DDRB = (1<<PORTB2)|(1<<PORTB1)|(1<<PORTB0); //Set Pin PB0,PB1(OC1A),PB2 to output
-	DDRD = (1<<PORTD6); //Set Pin PD6(OC0A) to output
-	//**************************************************************************************
-	//Configure Timer 1
-	//**************************************************************************************
-	//Setup Compare Output Mode for Channel A and Wave Generation Mode
-	TCCR1A=(0<<COM1A1)|(1<<COM1A0)|(0<<WGM11)|(0<<WGM10);
-	TCCR1B = (0<<WGM13)|(1<<WGM12)|(0<<CS12)|(1<<CS11)|(1<<CS10); //Define Clock Source Prescaler
-	OCR1A = 25000; //Set Output Compare A to match every 24999 clocks (24999x8uS =100mS)
-	TIMSK1 = (1<<OCIE1A); //Enable Interrupt for Output Compare A Match
+
+    //Configure I/O
+    //Set SDA (PC4) and SCL (PC5) as outputs for TWI bus
+    DDRC = (1<<PORTC5)|(1<<PORTC4);
+    //Configure PD0 as output for debug output
+    DDRD = (1<<PORTD0)|(1<<PORTD1)|(1<<PORTD2)|(1<<PORTD3);
+	DDRB = (1<<PORTB1);
+
+    //Configure Timer 1 to generate an ISR every 100ms
+    //No Pin Toggles, CTC Mode
+    TCCR1A = (0<<COM1A1)|(1<<COM1A0)|(0<<COM1B1)|(0<<COM1B0)|(0<<WGM11)|(0<<WGM01);
+    //CTC Mode, N=64
+    TCCR1B = (0<<ICNC1)|(0<<ICES1)|(0<<WGM13)|(1<<WGM12)|(0<<CS12)|(1<<CS11)|(1<<CS10);
+    //Enable Channel A Match Interrupt
+    TIMSK1 = (0<<ICIE1)|(0<<OCIE1B)|(1<<OCIE1A)|(0<<TOIE1);
+    //Setup output compare for channel A
+    OCR1A = 24999; //OCR1A = 16Mhz/N*Deltat-1 = 16Mhz/64*.1-1
+	//OCR1B = 6249; // 0.1s * (16 MHz / 256) - 1
+	
+    
+    //Configure TWI module (no interrupts)
+    //Configure Bit Rate (TWBR)
+    TWBR = 50;
+    //Configure TWI Interface (TWCR)
+    //TWINT: 0 - Interrupt Flag
+    //TWEA: 0 - No acknowledge Bit
+    //TWSTA: 0 - Start Condition Bit
+    //TWSTO: 0 - Stop Condition
+    //TWEN: 1 - Enable TWI Interface
+    //TWIE: 0 - Disable Interrupt
+    TWCR = (0<<TWINT)|(0<<TWEA)|(0<<TWSTO)|(0<<TWWC)|(1<<TWEN)|(1<<TWIE);
+    //Configure TWI Status Register (TWSR)
+    //TWS7-TWS3: 00000 - Don't change status
+    //TWPS1-TWPS0: 10 - Prescaler Value = 64
+    TWSR = (0<<TWS7)|(0<<TWS6)|(0<<TWS5)|(0<<TWS4)|(0<<TWS3)|(1<<TWPS1)|(0<<TWPS0);
+	
+	
+	// LAB 4 VV
+	
 	//**************************************************************************************
 	//Configure Timer 0
 	//**************************************************************************************
-	TCCR0A=(1<<COM0A1)|(0<<COM0A0)|(1<<WGM01)|(1<<WGM00);//Setup Fast PWM Mode for Channel A
-	TCCR0B = (0<<WGM02)|(1<<CS02)|(0<<CS01)|(1<<CS00); //Define Clock Source Prescaler
+	TCCR0A = (1 << COM0A1) | (0 << COM0A0) | (1 << WGM01) | (1 << WGM00);//Setup Fast PWM Mode for Channel A
+	TCCR0B = (0 << WGM02) | (1 << CS02) | (0 << CS01) | (1 << CS00); //Define Clock Source Prescaler
 	OCR0A = 64; //Initialized PWM Duty Cycle
 	//**************************************************************************************
 	//Configure A/D
 	//**************************************************************************************
 	//Select Analog Port 0 and Internal Reference Voltage;
-	ADMUX = (0<<REFS1)|(1<<REFS0)|(1<<ADLAR)|(0<<MUX3)|(0<<MUX2)|(0<<MUX1)|(0<<MUX0);
-	ADCSRA = (1<<ADEN)|(1<<ADIE)|(1<<ADPS2)|(1<<ADPS1)|(0<<ADPS0); //Enable A/D, Enable Interrupt, Set A/D Prescalar
-	DIDR0 = (1<<ADC0D); //Disable Input Buffers
-	ADCSRA |= (1<<ADSC); //Start Conversion
-	while(1) {
-		// toggle pin
+	ADMUX = (0 << REFS1) | (1 << REFS0) | (1 << ADLAR) | (0 << MUX3) | (0 << MUX2) | (0 << MUX1) | (0 << MUX0);
+	ADCSRA = (1 << ADEN) | (1 << ADIE) | (1 << ADPS2) | (1 << ADPS1) | (0 << ADPS0); //Enable A/D, Enable Interrupt, Set A/D Prescalar
+	DIDR0 = (1 << ADC0D); //Disable Input Buffers
+	ADCSRA |= (1 << ADSC); //Start Conversion
+	
+	// LAB 4 ^^
+
+	//Initialize Display
+	display_state=17;//Set state machine to initialization section
+	UpdateTWIDisplayState();//Run state machine
+	
+    sei();
+
+	OCR0A = 100;
+
+    /* Replace with your application code */
+    while (1) 
+    {
+        //Toggle Pin
 		PIND = (1<<PIND0);
 		
 		//Read Button
@@ -89,78 +130,81 @@ int main(void)
 			ButtonPressed[0] = OFF[0];
 			ButtonPressed[1] = OFF[1];
 			ButtonPressed[2] = OFF[2];
-			
-			SecondLineStr[13] = "0"; // <-- direction output
 		}
-		else{
+		else {
 			ButtonPressed[0] = ON[0];
 			ButtonPressed[1] = ON[1];
 			ButtonPressed[2] = ON[2];
-			
-			SecondLineStr[13] = "1"; // <-- direction output
+		}
+		
+		UpdateValues();
+    }
+}
+
+
+ISR(TIMER1_COMPA_vect)
+{
+    //Start New LCD Screen Update
+	display_state = 0;
+	isrHalfSecondCount += 1;
+	
+	if (isrHalfSecondCount > 4) {
+		isrHalfSecondCount = 0;
+		if(display_state==0) {
+			UpdateTWIDisplayState();
 		}
 	}
+	
+	time +=1;
+	
+	PORTB ^= 0x01; //Toggle Pin PD0 - this is a breadcrumb to say "did we get into ISR"
+	ADCSRA |= (1 << ADSC); //Start Conversion
 }
 
 void UpdateValues()
 {
-        /*FirstLineStr[8] = 0b00110000 | ( tenths_digit & 0b00001111);
-        SecondLineStr[9] = 0b00110000 | ( seconds_tens_digit & 0b00001111);
-        SecondLineStr[10] = 0b00110000 | ( seconds_ones_digit & 0b00001111);
-        ThirdLineStr[9] = 0b00110000 | ( minutes_tens_digit & 0b00001111);
-        ThirdLineStr[10] = 0b00110000 | ( minutes_ones_digit & 0b00001111);*/
-
-        FirstLineStr[4] = 0b00110000 | (adcValueOutputZero / 1000);
-        FirstLineStr[5] = 0b00110000 | ((adcValueOutputZero / 100) % 10);
-        FirstLineStr[6] = 0b00110000 | ((adcValueOutputZero / 10) % 10);
-        FirstLineStr[7] = 0b00110000 | (adcValueOutputZero % 10);
-
-        SecondLineStr[5] = 0b00110000 | (readADCL / 1000); // <-- motor duty cycle
-        SecondLineStr[6] = 0b00110000 | ((readADCL / 100) % 10); // <-- motor duty cycle
-        SecondLineStr[7] = 0b00110000 | ((readADCL / 10) % 10); // <-- motor duty cycle
-        //SecondLineStr[13] = direction; // <-- direction output
-
-        ThirdLineStr[8] = ButtonPressed[0];
-        ThirdLineStr[9] = ButtonPressed[1];
-        ThirdLineStr[10] = ButtonPressed[2];
+	
+	/*FirstLineStr[8] = 0b00110000 | ( tenths_digit & 0b00001111);
+	SecondLineStr[9] = 0b00110000 | ( seconds_tens_digit & 0b00001111);
+	SecondLineStr[10] = 0b00110000 | ( seconds_ones_digit & 0b00001111);
+	ThirdLineStr[9] = 0b00110000 | ( minutes_tens_digit & 0b00001111);
+	ThirdLineStr[10] = 0b00110000 | ( minutes_ones_digit & 0b00001111);*/
+	
+	FirstLineStr[3] = 0b00110000 | (adcValueOutputZero / 1000);
+	FirstLineStr[4] = 0b00110000 | ((adcValueOutputZero / 100) % 10);
+	FirstLineStr[5] = 0b00110000 | ((adcValueOutputZero / 10) % 10);
+	FirstLineStr[6] = 0b00110000 | (adcValueOutputZero % 10);
+	
+	// OCR0A = 100;
+	
+	SecondLineStr[3] = 0b00110000 | (OCR0A / 255);
+	SecondLineStr[4] = 0b00110000 | (OCR0A / 25);
+	SecondLineStr[5] = 0b00110000 | ((OCR0A % 25) % 25);
+	//SecondLineStr[10] = direction;
+	
+	ThirdLineStr[8] = ButtonPressed[0];
+	ThirdLineStr[9] = ButtonPressed[1];
+	ThirdLineStr[10] = ButtonPressed[2];
 }
 
-//Interrupt Routine for Timer 1 Match
-ISR (TIMER1_COMPA_vect) {
-	PORTB ^= 0x01; //Toggle Pin PB0
-	ADCSRA |= (1<<ADSC); //Start Conversion
-}
 
-ISR(TWI_vect)
+/************************************************************************/
+/* Display Functions                                                    */
+/************************************************************************/
+
+
+
+void DummyLoop(uint16_t count)
 {
-	PIND = (1<<PIND2);
-	//Read status register and mask out prescaler bits
-	uint8_t status = TWSR & 0xF8;
-
-	//Switch based on status of TWI interface
-	switch(status)
-	{
-		case 0x08: //Start Condition Transmitted
-			UpdateTWIDisplayState();
-			break;
-		case 0x10: //Repeat Start Condition Transmitted
-			UpdateTWIDisplayState();
-			break;
-		case 0x18: //SLA+W transmitted and ACK received
-			UpdateTWIDisplayState();
-			break;
-		case 0x20: //SLA+W transmitted and ACK NOT received
-			//This is an error, do something application specific
-			break;
-		case 0x28: //Data byte transmitted and ACK received
-			UpdateTWIDisplayState();
-			break;
-		case 0x30: //Data byte transmitted and ACK NOT received
-			//This is an error, do something application specific
-			break;
-	}
-	PIND = (1<<PIND2);
+    //Each index1 loop takes approx 100us 
+    for ( uint16_t index1=0; index1<=count; index1++ ) {
+        //Each index2 loop takes .5us (200 loops = 100us)
+        for ( uint16_t index2=0; index2<=200; index2++ ){
+            //Do nothing
+        }
+    }
 }
+
 
 void UpdateTWIDisplayState()
 {
@@ -252,7 +296,7 @@ void UpdateTWIDisplayState()
 				display_state++; //Send stop signal
 			}
 			TWCR = TWCR_SEND; //Set TWINT to send data
-			break;
+			break;					
 		case 13: //Send Fourth Line Characters
 			TWDR = FourthLineStr[TWI_char_index];
 			if(TWI_char_index<19) //If not last character
@@ -272,7 +316,7 @@ void UpdateTWIDisplayState()
 			break;
 		/************************************************************************/
 		/* Initialization States                                                */
-		/************************************************************************/
+		/************************************************************************/	
 		case 17: //Initialize Step One
 			DummyLoop(400);//Wait 40ms for powerup
 
@@ -288,9 +332,9 @@ void UpdateTWIDisplayState()
 			TWDR = 0x80; //Instruction 0x80
 			TWCR = TWCR_SEND; //Set TWINT to send data
 			display_state++;
-			break;
+			break;	
 		case 20: //Set Function Mode
-			TWDR = 0x38;
+			TWDR = 0x38; 
 			TWCR = TWCR_SEND; //Set TWINT to send data
 			display_state++;
 			break;
@@ -361,11 +405,46 @@ void UpdateTWIDisplayState()
 			TWCR = TWCR_STOP;//finish transaction
 			DummyLoop(1);//Delay 100us
 			display_state=0;
-			break;
+			break;					
 		default:
 			display_state = 0;
 	}
 
+}
+
+/************************************************************************/
+/* Define TWI Functions                                                 */
+/************************************************************************/
+
+ISR(TWI_vect)
+{
+	PIND = (1<<PIND2);
+	//Read status register and mask out prescaler bits
+	uint8_t status = TWSR & 0xF8;
+
+	//Switch based on status of TWI interface
+	switch(status)
+	{
+		case 0x08: //Start Condition Transmitted
+			UpdateTWIDisplayState();
+			break;
+		case 0x10: //Repeat Start Condition Transmitted
+			UpdateTWIDisplayState();		
+			break;
+		case 0x18: //SLA+W transmitted and ACK received
+			UpdateTWIDisplayState();
+			break;
+		case 0x20: //SLA+W transmitted and ACK NOT received
+			//This is an error, do something application specific
+			break;
+		case 0x28: //Data byte transmitted and ACK received
+			UpdateTWIDisplayState();
+			break;
+		case 0x30: //Data byte transmitted and ACK NOT received
+			//This is an error, do something application specific
+			break;
+	}
+	PIND = (1<<PIND2);
 }
 
 //Interrupt Routine for A/D Completion
@@ -377,6 +456,7 @@ ISR (ADC_vect) {
 	if (isrHalfSecondCount > 4) {
 		isrHalfSecondCount = 0;
 		if(display_state==0) {
+			OCR0A += 1;
 			UpdateTWIDisplayState();
 		}
 	}
@@ -385,7 +465,7 @@ ISR (ADC_vect) {
 	
 	PORTB ^= 0x04;//Toggle Pin PB2
 	readADCL = ADCL; // reading ADC low
-	OCR0A = ADCH; //Load A/D High Register in OCR0A to set PWM Duty Cycle
+	//OCR0A = ADCH; //Load A/D High Register in OCR0A to set PWM Duty Cycle
 	
 	// checks if last bit in ADMUX is set - go into MUX 0
 	/* if ((ADMUX & 0b00001111) == 0) {
@@ -403,3 +483,5 @@ ISR (ADC_vect) {
 		ADMUX = ADMUX & 0b11111110; // switch sensor reading
 	} */
 }
+
+
