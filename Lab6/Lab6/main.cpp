6ff5aac622a19412aa6e210a82663892edc157f7
TheRoyalIrishman <TheRoyalIrishman@users.noreply.github.com>
2024-03-18 21:32:02 -0400

    Lab 6 Project

---
+++ Lab6/Lab6/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Lab6.cpp
+ *
+ * Created: 3/18/2024 9:31:05 PM
+ * Author : CamsComputer
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
